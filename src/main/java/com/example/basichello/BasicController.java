64bc1adf95afb9b7e167dc27a0374b204bca9493
Karthikeyan Nadesan <karthik_krish_84@yahoo.com>
2022-10-06 23:25:02 -0400

    Allowed cross origin from react application

--- src/main/java/com/example/basichello/BasicController.java
+++ src/main/java/com/example/basichello/BasicController.java
@@ -1,6 +1,7 @@
 package com.example.basichello;
 
 import com.example.basichello.model.UserData;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/hello")
 public class BasicController {
+    @CrossOrigin("http://karthikreactapp.eastus.cloudapp.azure.com:80")
     @GetMapping("/world")
     List<UserData> hello() {
         System.out.println("I am accepting Request");
